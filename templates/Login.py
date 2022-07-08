f870373418f618e342a4eb3c8162fa2e2361c10c
Eric <erictoribio2010@gmail.com>
2022-07-07 22:17:37 -0400

    Login template complete

--- templates/Login.py
+++ templates/Login.py
@@ -1,3 +1,4 @@
+import tkinter as tk
 from tkinter import *
 from functools import partial
 def validateLogin(username, password):
@@ -6,24 +7,38 @@ def validateLogin(username, password):
     return
 
 
-window = Tk()
+root = tk.Tk()
 # set window title
-window.title("Shop POS")
+root.title("Shop POS")
 # set window width and height
-window.configure(width=500, height=300)
-# set window background color
-window.configure(bg='lightgray')
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+# f1= root.geometry("%dx%d" % (width, height))
+# f1.configure(bg='gray')
+f1 = tk.Frame(root.geometry("%dx%d" % (width, height)))
+f4 = tk.Frame(width=(width/4), height=(height/4), borderwidth = 1, relief = "solid", pady=20, padx=20)
+f3 = tk.Frame(width = 200, height = 50)
 
-usernameLabel= Label(window, text="User Name").grid(row=0, column=0)
+f1.pack(fill="both", expand=True, padx=20, pady=20)
+f4.place(in_=f1, anchor = "c", relx = .5, rely = .5)
+f3.place(in_ = f4, anchor = "c", relx = .5, rely =.6)
+
+border = Label(f4, text = "Sign in to Continue", pady =20, padx=20)
+border.place(in_ = f4, anchor = "c", relx = .5, rely = .1)
+
+welcome = Label(f1, text="Welcome to Toribio's POS", font=("Arial", 35))
+welcome.place(in_ = f1, anchor = "c", relx = .5, rely = .2)
+
+usernameLabel= Label(f3, text="User Name").grid(row=0, column=0, pady=(20,0), padx=20)
 username = StringVar()
-usernameEntry = Entry(window, textvariable=username).grid(row=0, column=1)
-passwordLabel = Label(window,text="Password").grid(row=1, column=0)  
+usernameEntry = Entry(f3, textvariable=username).grid(row=0, column=1, pady=(20,0), padx=20)
+passwordLabel = Label(f3,text=" Password  ").grid(row=1, column=0, pady=(20,0), padx=20)  
 password = StringVar()
-passwordEntry = Entry(window, textvariable=password, show='*').grid(row=1, column=1)  
+passwordEntry = Entry(f3, textvariable=password, show='*').grid(row=1, column=1, pady=(20,0), padx=20)  
 
 validateLogin = partial(validateLogin, username, password)
 
 #login button
-loginButton = Button(window, text="Login", command=validateLogin).grid(row=4, column=0) 
+loginButton = Button(f3, text="Login", command=validateLogin).grid(row=4, column=0, pady=20, padx=20) 
 
-window.mainloop()
+root.mainloop()
