41299261f8e06875e81ad8acc882ad5b33d1e846
Eric <erictoribio2010@gmail.com>
2022-07-11 11:26:12 -0400

    sign up page

--- flask_app/templates/login.py
+++ flask_app/templates/login.py
@@ -18,19 +18,27 @@ def validateLogin(name, password):
     username = name.get()
     loggedIn = LabelFrame(root, text="Hello " + username + " you are loggedIn", font=("Arial", 35))
     loggedIn.pack(fill='both', expand=1)
-    f4.forget()
+    sign_in_main_frame.forget()
     return 
-f4 = LabelFrame(root,width=(width/4), height=(height/4), borderwidth = 1, relief = "solid", pady=20, padx=20)
-f3 = Frame(width = 200, height = 50)
 
-f4.pack(fill = 'both', expand = 1)
-f3.place(in_ = f4, anchor = "c", relx = .5, rely =.6)
+def sign_up_form():
+    sign_up_main_frame.pack(fill='both', expand=1)
+    sign_in_main_frame.forget()
+    return
 
-welcome = Label(root, text="Welcome to Toribio's POS", font=("Arial", 35))
-welcome.place(in_ = root, anchor = "c", relx = .5, rely = .2)
+sign_in_main_frame = LabelFrame(root,width=width,height=height)
+sign_In_Frame = LabelFrame(sign_in_main_frame,width=(width/4), height=(height/4), borderwidth = 1, relief = "solid")
+f3 = Frame(width = 200, height = 100)
+border = Label(sign_In_Frame, text = "Sign in to Continue",)
+border.place(in_ = sign_In_Frame, anchor = "c", relx = .5, rely = .1)
 
-border = Label(f4, text = "Sign in to Continue", pady =20, padx=20)
-border.place(in_ = f4, anchor = "c", relx = .5, rely = .1)
+sign_in_main_frame.pack(fill='both', expand=1)
+sign_In_Frame.place(in_ = sign_in_main_frame, anchor="c", relx=.5,rely=.5)
+f3.place(in_ = sign_In_Frame, anchor = "c", relx = .5, rely =.6)
+
+welcome = Label(sign_in_main_frame, text="Welcome to Toribio's POS", font=("Arial", 35))
+welcome.place(in_ = sign_in_main_frame, anchor = "c", relx = .5, rely = .2)
+#-------login in form-------
 usernameLabel= Label(f3, text="User Name").grid(row=0, column=0, pady=(20,0), padx=20)
 username = StringVar()
 usernameEntry = Entry(f3, textvariable=username).grid(row=0, column=1, pady=(20,0), padx=20)
@@ -42,5 +50,11 @@ validateLogin = partial(validateLogin, username, password)
 
 #login button
 loginButton = Button(f3, text="Login", command=validateLogin).grid(row=4, column=0, pady=20, padx=20) 
+sign_up_btn = Button(f3, text="New Here? Sign Up!", command=sign_up_form,borderwidth=0,fg="blue",cursor="hand2").grid(row=4, column=1, pady=20, padx=20) 
+# sign up form
+sign_up_main_frame = LabelFrame(root,width=width,height=height)
+sign_up_frame = LabelFrame(sign_up_main_frame,width=(width/4), height=(height/4), borderwidth = 1, relief = "solid", pady=20, padx=20)
+sign_up_frame.place(in_ = sign_up_main_frame, anchor="c", relx=.5,rely=.5)
+
 
 root.mainloop()
